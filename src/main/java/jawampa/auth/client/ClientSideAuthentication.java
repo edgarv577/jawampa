a96d46da6b644e09e7589a87a2696d37aa6e6f1f
alejandro.miguez <alejandro.miguez@leapsight.com>
2022-12-15 14:53:19 -0300

    Add support for cryptosign auth method based on libsodium cryptography.

--- src/main/java/jawampa/auth/client/ClientSideAuthentication.java
+++ src/main/java/jawampa/auth/client/ClientSideAuthentication.java
@@ -4,8 +4,12 @@ import jawampa.WampMessages.AuthenticateMessage;
 import jawampa.WampMessages.ChallengeMessage;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public interface ClientSideAuthentication {
     String getAuthMethod();
     AuthenticateMessage handleChallenge( ChallengeMessage message, ObjectMapper objectMapper );
+    default ObjectNode getAuthExtra() {
+        return null;
+    };
 }
